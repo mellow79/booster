4025915df65ef5e2170726881f7b454bf4db98b6
Mark Lowery <mlow7920@gmail.com>
2017-11-03 03:49:43 -0400

    remove added forms

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -67,13 +67,28 @@ class RatingController extends Controller
   // adds review information the database
   public function addFundraiser(Request $request) {
 
-    $data = DB::table('fundraiser')->insert(
+    $id = DB::table('fundraiser')->insertGetId(
       ['fundraiser_name' => $request->new_fundraiser]
     );
 
-    $this->addReview($data->save());
 
-    if($data){
+    $reviewer_name = (isset($request->reviewer_name)) ? $request->reviewer_name : 'Anonymous';
+    $reviewer_email = (isset($request->reviewer_email)) ? $request->reviewer_email : '';
+    $review = (isset($request->review)) ? $request->review : '';
+    $review_date = date("Y-m-d H:i:s");
+
+    $d = DB::table('review')->insert(
+      [
+        'fid'=> $id
+        ,'rating'=> $request->rating
+        ,'reviewer_name' => $reviewer_name
+        ,'reviewer_email' => $reviewer_email
+        ,'review' => $review,
+        'review_date'=> $review_date]
+    );
+
+
+    if($d){
       $response_array['status'] = 'success';
     } else {
       $response_array['status'] = 'error';
